b3d7b509e2ed210caae1ae772d0352054c81ee6b
Arnold Chan <eagle17hk@gmail.com>
2024-11-16 16:54:25 +0800

    Added Dotween Pro and instructor features

--- Assets/Scripts/GymroomInstruction.cs
+++ Assets/Scripts/GymroomInstruction.cs
@@ -1,11 +1,64 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
+using TMPro;
 
 public class GymroomInstruction : MonoBehaviour {
-	public List<CanvasGroup> phaseCanvas = new List<CanvasGroup>();
+	[SerializeField] private List<CanvasGroup> phaseCanvasGroups = new List<CanvasGroup>();
+	private int currentPhaseIndex = 0;
+	[SerializeField] private float fadeOutDuration = 0.5f;
+	[SerializeField] private float fadeInDuration = 0.5f;
 
-	public void FadeToCanvas(int index) {
+	[SerializeField] private TextMeshProUGUI countText;
+	public float CountDurationOffset = 1f;
+
+	private void Awake() {
+		currentPhaseIndex = phaseCanvasGroups.FindIndex(x => x.gameObject.activeSelf);
+		if (currentPhaseIndex < 0) {
+			phaseCanvasGroups[0].gameObject.SetActive(true);
+			currentPhaseIndex = 0;
+		}
+		CountdownFrom(15);
+	}
+
+	public void Update() {
+		if (Input.GetKeyDown(KeyCode.Space)) {
+			FadeToNextCanvas(currentPhaseIndex + 1);
+		}
+	}
+
+	public void FadeToNextCanvas(int index) {
+		if (index >= phaseCanvasGroups.Count) {
+			Debug.Log(string.Format("Phase Index: {0} is not exist", index));
+			return;
+		}
+		int fadeOutIndex = currentPhaseIndex;
+		phaseCanvasGroups[currentPhaseIndex].DOFade(0f, fadeOutDuration)
+			.OnComplete(() => {
+				phaseCanvasGroups[fadeOutIndex].gameObject.SetActive(false);
+				currentPhaseIndex = index;
+				phaseCanvasGroups[currentPhaseIndex].gameObject.SetActive(true);
+				phaseCanvasGroups[currentPhaseIndex].DOFade(1f, fadeOutDuration);
+			});
+	}
+
+	public void CountdownFrom(int maxNumber) {
+		if (countText == null) return;
+		int countingNumber = maxNumber;
+		countText.text = maxNumber.ToString();
+		countText.gameObject.SetActive(true);
+		for (int i = 0; i < maxNumber; i++) {
+			countText.transform.DOScale(1f, CountDurationOffset)
+				.SetDelay(CountDurationOffset * i)
+				.OnComplete(() => {
+					countingNumber--;
+					countText.text = countingNumber.ToString();
+					if (countingNumber == 1) {
+						countText.gameObject.SetActive(false);
+					}
+				});
+		}
 
 	}
 }
