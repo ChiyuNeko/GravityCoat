f6e1c36068c902c3222d227b4d31978b521ff88d
Arnold Chan <eagle17hk@gmail.com>
2024-11-25 16:17:00 +0800

    Fixed Gymroom settings, Changed Luna's fonts, Added SceneChanger to both scenes

---
+++ Assets/Scripts/SceneChanger.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChanger : MonoBehaviour
+{
+	private void Update() {
+		if (Input.GetKeyDown(KeyCode.Alpha1)) {
+			SceneManager.LoadScene(0);
+		}
+		if (Input.GetKeyDown(KeyCode.Alpha2)) {
+			SceneManager.LoadScene(1);
+		}
+		if (Input.GetKeyDown(KeyCode.R)) {
+			SceneManager.LoadScene(0);
+		}
+	}
+}
