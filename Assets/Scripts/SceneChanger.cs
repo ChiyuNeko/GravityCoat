1f15aa49b0e0e7a3cc8a642e1f79a801e9823241
Arnold Chan <eagle17hk@gmail.com>
2024-11-25 16:35:34 +0800

    Add SceneChange BlackOverlay Fade

--- Assets/Scripts/SceneChanger.cs
+++ Assets/Scripts/SceneChanger.cs
@@ -2,18 +2,54 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using DG.Tweening;
 
-public class SceneChanger : MonoBehaviour
-{
+public class SceneChanger : MonoBehaviour {
+	public static SceneChanger Instance;
+	[SerializeField] private CanvasGroup blackOverlayCanvasGroup;
+	[SerializeField] private float fadeInDuration = 1f;
+	[SerializeField] private float fadeOutDuration = 1f;
+	private bool isSceneChanging = false;
+
+	private void Awake() {
+		if (SceneChanger.Instance == null) {
+			DontDestroyOnLoad(this.gameObject);
+			Instance = this;
+		} else {
+			Destroy(this.gameObject);
+		}
+	}
+
 	private void Update() {
 		if (Input.GetKeyDown(KeyCode.Alpha1)) {
-			SceneManager.LoadScene(0);
+			ChangeToScene(0);
 		}
 		if (Input.GetKeyDown(KeyCode.Alpha2)) {
-			SceneManager.LoadScene(1);
+			ChangeToScene(1);
 		}
 		if (Input.GetKeyDown(KeyCode.R)) {
-			SceneManager.LoadScene(0);
+			ChangeToScene(0);
 		}
+	}
+
+	public void ChangeToScene(int sceneIndex) {
+		if (isSceneChanging == true) return;
+		isSceneChanging = true;
+		blackOverlayCanvasGroup.gameObject.SetActive(true);
+		blackOverlayCanvasGroup.alpha = 0f;
+		blackOverlayCanvasGroup.DOFade(1f, fadeInDuration)
+			.OnComplete(() => {
+				SceneManager.LoadScene(sceneIndex);
+			});
+		SceneManager.sceneLoaded += FadeOutBlackOverlay;
+	}
+
+	private void FadeOutBlackOverlay(Scene scene, LoadSceneMode mode) {
+		isSceneChanging = false;
+		blackOverlayCanvasGroup.DOFade(0f, fadeOutDuration)
+			.OnComplete(() => {
+				blackOverlayCanvasGroup.gameObject.SetActive(false);
+			});
+		SceneManager.sceneLoaded -= FadeOutBlackOverlay;
 	}
 }
