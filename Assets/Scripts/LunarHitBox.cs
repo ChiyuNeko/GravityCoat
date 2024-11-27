ec35b4bb392a47c273c14c1cac1c2acf9f1263d4
ChiyuNeko <joe26121@gmail.com>
2024-11-28 04:04:01 +0800

    Update/Lunar Game Score UI

--- Assets/Scripts/LunarHitBox.cs
+++ Assets/Scripts/LunarHitBox.cs
@@ -2,16 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
+using TMPro;
 
 public class LunarHitBox : MonoBehaviour
 {
     public UnityEvent OnHitEvent;
     public UnityEvent ExitHitEvent;
+    Collider temp;
+    [SerializeField] int Total;
+    [SerializeField] int HitCount;
+    [SerializeField] int Score;
+
     void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag == "Stone")
         {
             OnHitEvent?.Invoke();
+            temp = other;
             Debug.Log("Hit");
         }
     }
@@ -23,4 +30,22 @@ public class LunarHitBox : MonoBehaviour
             Debug.Log("Exit");
         }
     }
+
+    public void TotalPlus()
+    {
+        Total++;
+    }
+    
+    public void HitCountPlus()
+    {
+        temp.gameObject.tag = "Untagged";
+    }
+
+    public void CauculateScore(TextMeshProUGUI text)
+    {
+        Score = Total - HitCount;
+        text.text ="Score: " + Score.ToString();
+    }
+
+
 }
