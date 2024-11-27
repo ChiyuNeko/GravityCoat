ee164277c2cb804c51f03f1fec09ea1aee050af9
ChiyuNeko <joe26121@gmail.com>
2024-11-27 14:53:12 +0800

    Update/Lunar Game & Lunar UI

--- Assets/Scripts/LunarHitBox.cs
+++ Assets/Scripts/LunarHitBox.cs
@@ -6,12 +6,21 @@ using UnityEngine.Events;
 public class LunarHitBox : MonoBehaviour
 {
     public UnityEvent OnHitEvent;
+    public UnityEvent ExitHitEvent;
     void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag == "Stone")
         {
             OnHitEvent?.Invoke();
-            Debug.Log("123123132132");
+            Debug.Log("Hit");
+        }
+    }
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Stone")
+        {
+            ExitHitEvent?.Invoke();
+            Debug.Log("Exit");
         }
     }
 }
